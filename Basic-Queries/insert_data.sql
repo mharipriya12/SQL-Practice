89d5acd9ead2ff04684205d3973e203bbd4839fb
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 16:03:07 +0530

    Add SQL script to insert student data

---
+++ Basic-Queries/insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO students (id, name, age, department, marks)
+VALUES
+(1, 'Arun', 20, 'CSE', 85),
+(2, 'Priya', 21, 'ECE', 90),
+(3, 'Karthik', 20, 'CSE', 78),
+(4, 'Divya', 22, 'EEE', 92),
+(5, 'Rahul', 21, 'ECE', 75);
